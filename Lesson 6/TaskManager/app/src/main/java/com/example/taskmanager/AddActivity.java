bdc20895596c00ef11355245ada0b7827e4864f5
18016599 <18016599@rp.edu.sg>
2020-05-28 20:34:43 +0800

    .

--- Lesson 6/TaskManager/app/src/main/java/com/example/taskmanager/AddActivity.java
+++ Lesson 6/TaskManager/app/src/main/java/com/example/taskmanager/AddActivity.java
@@ -1,15 +1,25 @@
 package com.example.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -31,20 +41,37 @@ public class AddActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = etName.getText().toString();
-                String desc = etDesc.getText().toString();
-                if (!name.isEmpty() && !desc.isEmpty()) {
+                String description = etDesc.getText().toString();
+                if (!name.isEmpty() && !description.isEmpty()) {
                     DBHelper dbhelper = new DBHelper(AddActivity.this);
-                    dbhelper.insertTask(name, desc);
+                    dbhelper.insertTask(name, description);
                 }
-                etName.setText("");
-                etDesc.setText("");
                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(etName.getWindowToken(), 0);
                 imm.hideSoftInputFromWindow(etDesc.getWindowToken(), 0);
 
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(AddActivity.this,
+                        uwuReceiver.class);
+                String[] info = {name, description};
+                intent.putExtra("info", info);
+                sendBroadcast(intent);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        AddActivity.this, 12345,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+
+                etName.setText("");
+                etDesc.setText("");
                 Intent returnIntent = new Intent();
                 setResult(RESULT_OK, returnIntent);
-
                 finish();
             }
         });
