f0ab6dc829bc92a901074e6df32db0f64a597dc4
18016599 <18016599@rp.edu.sg>
2020-07-16 09:58:32 +0800

    .

--- Lesson 9/DemoLocationDetection/app/src/main/java/sg/edu/rp/demolocationdetection/MainActivity.java
+++ Lesson 9/DemoLocationDetection/app/src/main/java/sg/edu/rp/demolocationdetection/MainActivity.java
@@ -13,6 +13,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnGetLastLocation, btnGetLocationUpdate, btnRemoveLocationUpdate;
     FusedLocationProviderClient client;
+    LocationRequest mLocationRequest;
+    LocationCallback mLocationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
         btnGetLocationUpdate = (Button) findViewById(R.id.btnGetLocationUpdate);
         btnRemoveLocationUpdate = (Button) findViewById(R.id.btnRemoveLocationUpdate);
         client = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setSmallestDisplacement(100);
+
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location data = locationResult.getLastLocation();
+                    double lat = data.getLatitude();
+                    double lng = data.getLongitude();
+                }
+            };
+        };
+
 
         btnGetLastLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,22 +83,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (checkPermission() == true) {
-                    Task<Location> task = client.getLastLocation();
-                    task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Check last known location. Can be null.
-                            if (location != null) {
-                                String msg = "Lat : " + location.getLatitude() +
-                                        " Lng : " + location.getLongitude();
-                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                            } else {
-                                String msg = "No Last Known Location Found";
-                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
                 }
+            }
+        });
+
+        btnRemoveLocationUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                client.removeLocationUpdates(mLocationCallback);
             }
         });
     }
