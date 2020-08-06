cc88bc8c84b4e6b53469df9dbaa88d5b3fee149e
18016599 <18016599@rp.edu.sg>
2020-08-06 11:10:20 +0800

    .

---
+++ Lesson 11/DemoNavigation/app/src/main/java/sg/edu/rp/demonavigation/NewActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.demonavigation;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+}
